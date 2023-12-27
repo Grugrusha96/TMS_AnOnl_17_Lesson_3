787f9299004e866b3e9516c0543083c46a3c7a61
Aleksey Grushenkov <153802645+Grugrusha96@users.noreply.github.com>
2023-12-27 16:09:46 +0300

    integer

--- src/Main.java
+++ src/Main.java
@@ -140,12 +140,19 @@
 //        System.out.println("Итоговая сумма: " + formattedSum);
 //    }
 //}
+//public class Main {
+//    public static void main(String[] args) {
+//        for(int i = 1; i <= 99; i++){
+//            if (i % 2 == 1){
+//                System.out.println(i);
+//            }
+//        }
+//    }
+//}
 public class Main {
     public static void main(String[] args) {
-        for(int i = 1; i <= 99; i++){
-            if (i % 2 == 1){
-                System.out.println(i);
-            }
+        for (int i = 5; i >= 1; i--){
+            System.out.println(i);
         }
     }
 }
